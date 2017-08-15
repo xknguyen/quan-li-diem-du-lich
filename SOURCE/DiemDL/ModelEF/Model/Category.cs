6edff715e34fbfdfd5ec46490b0ee44d2f04ceea
Sang Khánh Vinh <khanhvinhit@gmail.com>
2017-08-15 11:15:09 +0700

    Source and Data

---
+++ SOURCE/DiemDL/ModelEF/Model/Category.cs
@@ -0,0 +1,37 @@
+namespace ModelEF.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Category")]
+    public partial class Category
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public long ID { get; set; }
+
+        [StringLength(250)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string MetaTitle { get; set; }
+
+        public int? DisplayOrder { get; set; }
+
+        public DateTime? CreateDate { get; set; }
+
+        public bool Status { get; set; }
+
+        public long? AccountID { get; set; }
+
+        [StringLength(250)]
+        public string MetaKeywords { get; set; }
+
+        [StringLength(250)]
+        public string MetaDescriptions { get; set; }
+
+        public virtual Account Account { get; set; }
+    }
+}
