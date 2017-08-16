7a28212d146000ddf6c90eae7caf5877e0a8f060
PhamThiGiang <30859593+PhamThiGiang@users.noreply.github.com>
2017-08-16 09:56:28 +0700

    Add files via upload

---
+++ SOURCE/DiemDL/DiemDL/Areas/Administrator/Controllers/MenusController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ModelEF.Model;
+
+namespace DiemDL.Areas.Administrator.Controllers
+{
+    public class MenusController : Controller
+    {
+        private DiemDLDBContext db = new DiemDLDBContext();
+
+        // GET: /Administrator/Menus/
+        public ActionResult Index()
+        {
+            var menus = db.Menus.Include(m => m.Account);
+            return View(menus.ToList());
+        }
+
+        // GET: /Administrator/Menus/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Menu menu = db.Menus.Find(id);
+            if (menu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menu);
+        }
+
+        // GET: /Administrator/Menus/Create
+        public ActionResult Create()
+        {
+            ViewBag.AccountID = new SelectList(db.Accounts, "ID", "UserName");
+            return View();
+        }
+
+        // POST: /Administrator/Menus/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,Icon,Text,Link,ParentID,DisplayOrder,Target,CreateDate,AccountID,Status")] Menu menu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Menus.Add(menu);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AccountID = new SelectList(db.Accounts, "ID", "UserName", menu.AccountID);
+            return View(menu);
+        }
+
+        // GET: /Administrator/Menus/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Menu menu = db.Menus.Find(id);
+            if (menu == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AccountID = new SelectList(db.Accounts, "ID", "UserName", menu.AccountID);
+            return View(menu);
+        }
+
+        // POST: /Administrator/Menus/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,Icon,Text,Link,ParentID,DisplayOrder,Target,CreateDate,AccountID,Status")] Menu menu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(menu).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AccountID = new SelectList(db.Accounts, "ID", "UserName", menu.AccountID);
+            return View(menu);
+        }
+
+        // GET: /Administrator/Menus/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Menu menu = db.Menus.Find(id);
+            if (menu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menu);
+        }
+
+        // POST: /Administrator/Menus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Menu menu = db.Menus.Find(id);
+            db.Menus.Remove(menu);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
