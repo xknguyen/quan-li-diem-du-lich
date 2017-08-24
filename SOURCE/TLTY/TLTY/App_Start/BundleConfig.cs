d6d59aa809800aa085623dcc2845ae5e2ec270f2
Anh Quân <30859330+quannbqa@users.noreply.github.com>
2017-08-24 14:54:29 +0700

    14:54

--- SOURCE/TLTY/TLTY/App_Start/BundleConfig.cs
+++ SOURCE/TLTY/TLTY/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace TLTY
 		public static void RegisterBundles(BundleCollection bundles)
 		{
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-						"~/Scripts/jquery-{version}.js"));
+						"~/Content/ClientTheme/js/jquery-{version}.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
 						"~/Scripts/jquery.validate*"));
@@ -20,12 +20,10 @@ namespace TLTY
 						"~/Scripts/modernizr-*"));
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-					  "~/Scripts/bootstrap.js",
-					  "~/Scripts/respond.js"));
+                        "~/Content/ClientTheme/js/bootstrap.min.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
-					  "~/Content/bootstrap.css",
-					  "~/Content/site.css"));
+					  "~/Content/ClientTheme/css/bootstrap.css"));
 
 
 			//admin
