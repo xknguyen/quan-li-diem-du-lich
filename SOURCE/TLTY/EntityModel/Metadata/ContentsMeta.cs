187a258c44f1bcb4ed1222dd8c6d149a67defba3
Anh Quân <30859330+quannbqa@users.noreply.github.com>
2017-08-29 16:38:38 +0700

    4:38 Content

---
+++ SOURCE/TLTY/EntityModel/Metadata/ContentsMeta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityModel.EF
+{
+    public class ContentsMeta
+    {
+        [AllowHtml]
+        public string Detail { get; set; }
+    }
+    [MetadataType(typeof(ContentsMeta))]
+    public partial class Content
+    {
+
+    }
+}
