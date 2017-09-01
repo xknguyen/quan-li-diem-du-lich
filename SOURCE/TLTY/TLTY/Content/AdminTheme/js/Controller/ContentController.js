36f1730eb88f2f2f3684869da647c8acc88df944
Anh Quân <30859330+quannbqa@users.noreply.github.com>
2017-09-01 15:36:45 +0700

    3:36

--- SOURCE/TLTY/TLTY/Content/AdminTheme/js/Controller/ContentController.js
+++ SOURCE/TLTY/TLTY/Content/AdminTheme/js/Controller/ContentController.js
@@ -7,6 +7,7 @@
             e.preventDefault();
             var btn = $(this);
             var id = btn.data('id');
+            var div = document.getElementById('thongbao');
             $.ajax({
                 url: "/Admin/Contents/ChangeStatus",
                 data: { id: id },
@@ -14,18 +15,18 @@
                 type: "POST",
                 success: function (response) {
                     console.log(response);
+                    div.style.display = 'block';
                     if (response.status == true) {
                         btn.text('Kích hoạt');
+                        $('.abc').html("<i class='fa fa-check'></i> Kích hoạt thành công.");
                     }
                     else {
-                        btn.text('Khóa');
+                        btn.text('Khoá');
+                        $('.abc').html("<i class='fa fa-check'></i> Khóa thành công.");
                     }
                 }
             });
         });
     }
-
-
-
 }
 user.init();
--- SOURCE/TLTY/TLTY/Content/AdminTheme/js/Controller/SliderController.js
+++ SOURCE/TLTY/TLTY/Content/AdminTheme/js/Controller/SliderController.js
@@ -7,25 +7,26 @@
 			e.preventDefault();
 			var btn = $(this);
 			var id = btn.data('id');
+			var div = document.getElementById('thongbao');
 			$.ajax({
 				url: "/Admin/Sliders/ChangeStatus",
 				data: { id: id },
 				dataType: "json",
 				type: "POST",
 				success: function (response) {
-					console.log(response);
+				    console.log(response);
+				    div.style.display = 'block';
 					if (response.status == true) {
-						btn.text('Kích hoạt');
+					    btn.text('Kích hoạt');
+					    $('.abc').html("<i class='fa fa-check'></i> Kích hoạt thành công.");
 					}
 					else {
-						btn.text('Khoá');
+					    btn.text('Khoá');
+					    $('.abc').html("<i class='fa fa-check'></i> Khóa thành công.");
 					}
 				}
 			});
 		});
 	}
-
-
-
 }
 user.init();
