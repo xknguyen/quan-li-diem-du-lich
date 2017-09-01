868ea3c0730176fa7769fd8f614b3ce5430b1d06
Sang Khánh Vinh <khanhvinhit@gmail.com>
2017-09-01 15:39:48 +0700

    css

---
+++ SOURCE/TLTY/TLTY/Controllers/NewsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TLTY.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: News
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		// GET: Admin/AccountGroups/Details/5
+		public ActionResult Details(long? id)
+		{
+			return View();
+		}
+    }
+}
