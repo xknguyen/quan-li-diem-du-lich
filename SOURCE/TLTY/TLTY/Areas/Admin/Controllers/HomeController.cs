d5f6ab16d17e16ff445e814aad3da851c61912f2
Sang Khánh Vinh <khanhvinhit@gmail.com>
2017-09-07 22:59:56 +0700

    fix

--- SOURCE/TLTY/TLTY/Areas/Admin/Controllers/HomeController.cs
+++ SOURCE/TLTY/TLTY/Areas/Admin/Controllers/HomeController.cs
@@ -5,21 +5,32 @@ using System.Web;
 using System.Web.Mvc;
 
 using System.Web.Services;
+using EntityModel.EF;
 
 namespace TLTY.Areas.Admin.Controllers
 {
-    public class HomeController : BaseController
-    {
-        // GET: Admin/Home
-        public ActionResult Index()
-        {
-            return View();
-        }
+	public class HomeController : BaseController
+	{
+		TLTYDBContext _db = new TLTYDBContext();
+		// GET: Admin/Home
+		public ActionResult Index()
+		{
+			return View();
+		}
 
 		public ActionResult NotificationAuthorize()
 		{
 			return View();
 		}
-    }
+
+		public JsonResult FeedbackMe()
+		{
+			var feedback = _db.Feedbacks.Where(x => x.Status == false).OrderByDescending(x => x.CreateDate).ToList();
+			return Json(new
+			{
+				data = feedback
+			}, JsonRequestBehavior.AllowGet);
+		}
+	}
 
 }
