16d5fb10166975f213a7b3ef8bf0f7c150279d49
Sang Khánh Vinh <khanhvinhit@gmail.com>
2017-09-07 16:53:39 +0700

    Fix Lỗi

--- SOURCE/TLTY/TLTY/Controllers/HomeController.cs
+++ SOURCE/TLTY/TLTY/Controllers/HomeController.cs
@@ -5,14 +5,22 @@ using System.Web;
 using System.Web.Mvc;
 
 using System.Web.Services;
+using EntityModel.EF;
+using System.Configuration;
 
 namespace TLTY.Controllers
 {
 	public class HomeController : Controller
 	{
+		TLTYDBContext _db = new TLTYDBContext();
 		public ActionResult Index()
 		{
-			return View();
+			ViewBag.Title = ConfigurationManager.AppSettings["HomeTitle"];
+			ViewBag.Keywords = ConfigurationManager.AppSettings["HomeKeyword"];
+			ViewBag.Descriptions = ConfigurationManager.AppSettings["HomeDescription"];
+			var listInstruction =
+				_db.Instructions.Where(x => x.Status == true).OrderByDescending(x => x.CreateDate).Take(8).ToList();
+			return View(listInstruction);
 		}
 
 		[WebMethod]
