061fd4027fb526b8255dd2df38ece32f806c6609
Sang Khánh Vinh <khanhvinhit@gmail.com>
2017-09-11 16:07:24 +0700

    fix

--- SOURCE/TLTY/TLTY/Controllers/HomeController.cs
+++ SOURCE/TLTY/TLTY/Controllers/HomeController.cs
@@ -13,15 +13,58 @@ namespace TLTY.Controllers
 	public class HomeController : Controller
 	{
 		TLTYDBContext _db = new TLTYDBContext();
-		public ActionResult Index()
+		public ActionResult Index(string nameThemes)
 		{
 			ViewBag.Title = ConfigurationManager.AppSettings["HomeTitle"];
 			ViewBag.Keywords = ConfigurationManager.AppSettings["HomeKeyword"];
 			ViewBag.Descriptions = ConfigurationManager.AppSettings["HomeDescription"];
-			var intruction = _db.Instructions.Where(x => x.Status == true).OrderByDescending(x => x.CreateDate).Take(8).ToList();
+
+            HttpCookie themePage = new HttpCookie("themePage", nameThemes);
+            themePage.Expires = DateTime.Now.AddMonths(1);
+            int response = 1;
+            if (themePage != null)
+            {
+                if (themePage.Value == "Den")
+                {
+                    response = 2;
+                }
+                else
+                {
+                    response = 1;
+                }
+            }
+            ViewBag.Views = response;
+
+            var intruction = _db.Instructions.Where(x => x.Status == true).OrderByDescending(x => x.CreateDate).Take(8).ToList();
 			return View(intruction);
 		}
 
 
-	}
+        public JsonResult ChangeTheme(string nameThemes)
+        {
+            HttpCookie themePage = new HttpCookie("themePage", nameThemes);
+            themePage.Expires = DateTime.Now.AddMonths(1);
+            int response = 1;
+            if (themePage != null)
+            {
+                if (themePage.Value == "Den")
+                {
+                    response = 2;
+                }
+                else
+                {
+                    response = 1;
+                }
+            }
+            return Json(response);
+        }
+
+		public PartialViewResult Slider()
+		{
+			var slider = _db.Sliders.Where(x => x.Status == true).OrderBy(x => x.DisplayOrder).ToList();
+			ViewBag.Content = _db.Contents.Where(x => x.Status == true).ToList();
+			return PartialView(slider);
+		}
+
+    }
 }
