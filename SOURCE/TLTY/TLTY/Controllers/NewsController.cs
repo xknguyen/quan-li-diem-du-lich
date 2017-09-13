d194ab20c76b6f5c004153dd5a77f372569e2231
PhamThiGiang <30859593+PhamThiGiang@users.noreply.github.com>
2017-09-13 16:19:18 +0700

    Cập nhật 4:19

--- SOURCE/TLTY/TLTY/Controllers/NewsController.cs
+++ SOURCE/TLTY/TLTY/Controllers/NewsController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EntityModel.EF;
 
 namespace TLTY.Controllers
 {
     public class NewsController : Controller
     {
+        TLTYDBContext _db = new TLTYDBContext();
         // GET: News
         public ActionResult Index()
         {
-            return View();
+            var news = _db.Contents.Where(x => x.Status&& x.Category).OrderByDescending(x=>x.CreateDate).ToList();
+            return View(news);
         }
 
 		// GET: Admin/AccountGroups/Details/5
